69133c138fa9a13319e09cbf4a375c6b5ade1af8
MadushaHero <madushalakmal@gmail.com>
2021-07-16 13:04:02 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/www/aiyo/main_dart_file/MainActivity.kt
@@ -0,0 +1,6 @@
+package www.aiyo.main_dart_file
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+main() {
+  
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+
+    return MaterialApp(
+
+      home: Text("Hello World"),
+    );
+
+
+  }
+
+
+}
+
